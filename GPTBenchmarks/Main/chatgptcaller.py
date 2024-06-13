d592e274f51877ecc948f7a5195f65c70d67da8b
fluflo11 <florentinroyer@gmail.com>
2024-06-13 15:39:42 +0200

    chatgptcaller working properly

--- GPTBenchmarks/Main/chatgptcaller.py
+++ GPTBenchmarks/Main/chatgptcaller.py
@@ -6,17 +6,25 @@ import subprocess
 client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
 
 
-def call_chatgpt(prompt):
+def call_chatgpt(prompt,tex_file,sysgoal):
 
-
-    response = client.completions.create(engine="gpt-3.5-turbo",
-    prompt=prompt,
+    messages_llm = [{"role": "system", "content":sysgoal},
+                    {"role":"user", "content":tex_file},
+                    {"role": "user", "content":prompt}]
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages = messages_llm,
     max_tokens=2048,
     n=1,
     stop=None,
-    temperature=0.2)
+    temperature=0.2,
+    stream=False
+    )
+    return response.choices[0].message.content
 
-    return response.choices[0].text.strip()
+
+
+
 
 def main(tex_file, prompt_file):
 
@@ -25,13 +33,15 @@ def main(tex_file, prompt_file):
     with open(prompt_file, 'r') as file:
         prompt_content = file.read()
 
-    prompt = f"{prompt_content}\n\n{tex_content}"
-    result = call_chatgpt(prompt)
-
+    sysgoal = "You are a helpful assistant for programming and customizing code. All you have to do is answer the question by writing the entire code with the modifications. To modify the code, use the comments. DO NOT answer anything other than the entire code. If you make mistakes, don't apologize, just send the entire code with the modifications"
+    result = call_chatgpt(prompt_content,tex_content,sysgoal)
+    
+    
     os.makedirs('Ressources', exist_ok=True)
-    with open('Ressources/Tikz/tikz_res.tex', 'w') as file: #res
+    with open('../Ressources/Tikz/tikz_res.tex', 'w') as file:
         file.write(result)
-    subprocess.run('python3 tikzcompiler.py Ressources/Tikz/tikz_res.tex')
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
